59f242a003980c79cceaf7136ca90f8506679b4b
Yasmin Aline Amaral <98675367+yasmin-aline@users.noreply.github.com>
2024-04-05 13:39:43 -0300

    exercicio3-controle-de-acesso

---
+++ Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora {
+
+    public Calculadora() {
+        System.out.println("Bem-vindo à calculadora!");
+    }
+
+    public Calculadora(int n1, int n2) {
+        int resultado = n1 + n2;
+        System.out.println("Resultado da soma: " + resultado);
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+//3 - Desenvolva uma classe que contenha 2(dois) construtores,
+// o primeiro deve mostrar uma mensagem que você desejar e a outra deve receber dois parâmetros
+// (int n1 e int n2) faça o cálculo e retorne o resultado para o usuário.
+
 public class Main {
     public static void main(String[] args) {
-
-                Mensagem mensagem = new Mensagem();
+                Calculadora calculadora1 = new Calculadora();
+                Calculadora calculadora2 = new Calculadora(150, 20);
 
             }
 
