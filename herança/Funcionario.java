3b7e3437f1877655e5279eb17cef7a6c4599059c
Yasmin Aline Amaral <98675367+yasmin-aline@users.noreply.github.com>
2024-04-09 01:22:54 -0300

    Add files via upload

---
+++ herança/Funcionario.java
@@ -0,0 +1,11 @@
+package heranca;
+public class Funcionario extends Pessoa{
+
+    public Funcionario(String fnome, String fcpf, int fidade ){
+        super(fnome, fcpf, fidade);
+    }
+public int salario;
+    public String departament;
+
+
+}
---
+++ herança/Pessoa.java
@@ -0,0 +1,27 @@
+package heranca;
+
+public class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private int idade;
+
+    public Pessoa(String nome, String cpf, int idade) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+}
